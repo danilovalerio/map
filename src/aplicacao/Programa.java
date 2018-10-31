12a24d28f7986750cd9779e17680f5e41765b4ca
danilovalerio <danilo_vs@hotmail.com>
2018-10-31 16:03:32 -0300

    Lista o Map com um item removido

--- src/aplicacao/Programa.java
+++ src/aplicacao/Programa.java
@@ -1,8 +1,24 @@
 package aplicacao;
 
+import java.util.Map;
+import java.util.TreeMap;
+
 public class Programa {
 
 	public static void main(String[] args) {
+		
+		//Exemplo do uso de Map
+		Map <String, String> cookies = new TreeMap<>(); //lembrando que TreeMap retorna ordenado pela chave que é String
+		cookies.put("username", "Maria");
+		cookies.put("email", "maria@marie.com");
+		cookies.put("fone", "987654321");
+		
+		cookies.remove("email"); //remove de acordo com a chave
+		
+		System.out.println("Todos os cookies:");
+		for(String key : cookies.keySet()) { //retorna um Sset com todas as key de um Map
+			System.out.println(key + " : "+ cookies.get(key));
+		}
 		
 	
 	}

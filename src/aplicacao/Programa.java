f825793690280b1173da3d3281ae9d0c8815d88a
danilovalerio <danilo_vs@hotmail.com>
2018-10-31 16:17:51 -0300

    Demonstrações de put, containsKey, remove, get e size

--- src/aplicacao/Programa.java
+++ src/aplicacao/Programa.java
@@ -12,15 +12,32 @@ public class Programa {
 		cookies.put("username", "Maria");
 		cookies.put("email", "maria@marie.com");
 		cookies.put("fone", "987654321");
+		//remove de acordo com a chave
+		cookies.remove("email");
+		System.out.println("Chave email foi removida");
 		
-		cookies.remove("email"); //remove de acordo com a chave
+		//como já tem a chave fone ele só substitui o valor
+		cookies.put("fone", "901001001");
 		
-		cookies.put("fone", "000000000"); //como já tem a chave fone ele só substitui o valor
+		//verifica se existe a chave 'fone'
+		System.out.println("Contém a chave 'fone': "+ cookies.containsKey("fone"));
 		
+		//pegando o valor de uma chave
+		System.out.println("Telefone: "+cookies.get("fone"));
+		
+		//solicitando uma chave que não existe
+		System.out.println("Email: "+ cookies.get("email"));
+		
+		//verificando o tamanho do Map
+		System.out.println("Tam. do Map: "+ cookies.size());
+				
 		System.out.println("Todos os cookies:");
-		for(String key : cookies.keySet()) { //keySet > retorna um Set com todas as key de um Map
+		//keySet > retorna um Set com todas as key de um Map e pega a chave com o .get(key)
+		for(String key : cookies.keySet()) { 
 			System.out.println(key + " : "+ cookies.get(key));
 		}
+		
+		
 		
 		
 		
